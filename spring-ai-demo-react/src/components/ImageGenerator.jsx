56e58acf1b393cd66e1c3c68ca5290eb7613c907
gary1116 <garygodwin234@gmail.com>
2026-03-20 08:18:27 +0530

    updated imagGenerator, added webConfig to resolve cors mapping issue

--- spring-ai-demo-react/src/components/ImageGenerator.jsx
+++ spring-ai-demo-react/src/components/ImageGenerator.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import {useState} from 'react';
+import axios from 'axios';
+
 
 const ImageGenerator = () => {
+    const [prompt,setPrompt]=useState('');
+    const [imageUrl,setImageUrl]=useState('');
+
+    const handleClick=async (prompt)=>{
+        try{
+        const response = await axios.get('http://localhost:8080/generate-image', {
+        params: { prompt }});
+        console.log(response.data);
+        setImageUrl(response.data);
+
+        }catch(error){
+            console.error('Error generating image',error);
+        }
+
+    }
+
   return (
-    <div>
-            <h1>ImageGenerator</h1>
+
+    <div className="flex gap-5 flex-col items-center justify-center flex-1 w-full px-4">
+
+    <div className="border w-1/2 h-1/2">{imageUrl}</div>
+    <div className=" items-center justify-center gap-2 flex w-full">
+    <input type="text"
+    value={prompt}
+    onChange={(e)=>setPrompt(e.target.value)}
+    placeholder='please Enter a prompt for image generation'
+    className='border-3 rounded-lg p-2 border-gray-500 w-3/4' 
+    />
+    <button className='cursor-pointer' onClick={()=>handleClick(prompt)}>Enter</button>
+    </div>
     </div>
   )
 }
